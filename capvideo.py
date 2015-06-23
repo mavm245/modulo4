e59f747fa47989a5a4d34528519a1b81b65b82f7
mvasquezbec <mavmfi@hotmail.com>
2015-06-23 10:46:07 -0500

    Captura video: solo tiene parametro que recibe para segundos de video

---
+++ capvideo.py
@@ -0,0 +1,25 @@
+###############################
+#  Modulo captura de video    #
+#  Proyecto de Modulo 4       #
+#	 		      #
+#      nmorales               #
+#      varteaga               #
+#      mvasquez               #
+###############################
+
+#!/usr/bin/python
+import os
+import datetime
+import sys
+
+x = datetime.datetime.now()
+x = x.isoformat()
+if len(sys.argv) >= 2:
+	y = sys.argv[1]
+	comando = "streamer -t 0:0:"+ y + " -c /dev/video0 -f rgb24 -r 3 -o " + x + ".avi"
+	print(comando)
+else:
+        print "Este programa necesita un parametro"
+
+os.system(comando)
+
