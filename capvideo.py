4643a3ab32054d2ec79445b9b148f2f406f58619
system admin <sysadmin@debian.debian_domain>
2015-06-25 17:17:26 -0500

    Captura de video con audio arreglado

--- capvideo.py
+++ capvideo.py
@@ -16,10 +16,10 @@ x = datetime.datetime.now()
 x = x.isoformat()
 if len(sys.argv) >= 2:
 	y = sys.argv[1]
-	comando = "streamer -t 0:0:"+ y + " -c /dev/video0 -f rgb24 -r 3 -o " + x + ".avi"
-	print(comando)
+	comando = "streamer -t 0:0:"+ y + " -c /dev/video0 -f rgb24 -F mono16 -r 3 -o " + x + ".avi"
+	#print(comando)
 else:
-        print "Este programa necesita un parametro"
+        print "Este programa necesita un parametro que indique el tiempo (Segundos)"
 
 os.system(comando)
 
