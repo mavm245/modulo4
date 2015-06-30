d5f753c549e7800f6aae1d7a6489b04054fbe240
nmoralesbec <nayemoralesp@hotmail.com>
2015-06-29 19:04:11 -0500

    Captura audio actualizacion

--- capaudio.py
+++ capaudio.py
@@ -20,8 +20,3 @@ def audio(arg1):
 	comando = "arecord -d " + y +" -f cd -t wav "+ x + ".wav"
 	os.system(comando)
 
-if len(sys.argv) == 2:
-	arg1 = sys.argv[1]
-	audio(arg1)
-else:
-        print "Este programa necesita un parametro"
