edec7766460de63178372e99034ef4e71488bfac
nmoralesbec <nayemoralesp@hotmail.com>
2015-06-29 19:00:43 -0500

    Actualizando capvideo

--- capvideo.py
+++ capvideo.py
@@ -20,8 +20,3 @@ def video(arg1):
 	comando = "streamer -t 0:0:"+ y + " -c /dev/video0 -f rgb24 -F mono16 -r 3 -o " + x + ".avi"
 	os.system(comando)
 
-if len(sys.argv) == 2:
-	arg1 = sys.argv[1]
-	video(arg1)
-else:
-	print "Este programa necesita un parametro que indique el tiempo (Segundos)"
