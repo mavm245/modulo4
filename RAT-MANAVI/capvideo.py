88d2d4c1fedcc8ff3bab0d94b80033758567e3d7
mvasquezbec <mavmfi@hotmail.com>
2015-07-23 23:17:39 -0500

    cambios para debian 6

--- RAT-MANAVI/capvideo.py
+++ RAT-MANAVI/capvideo.py
@@ -17,6 +17,8 @@ def video(arg1):
 	x = datetime.datetime.now()
 	x = x.isoformat()
 	y = arg1
-	comando = "streamer -t 0:0:"+ y + " -c /dev/video0 -f rgb24 -F mono16 -r 3 -o " + x + ".avi"
-	os.system(comando)
-
+#debian 7 y 8
+#       comando = "streamer -t 0:0:"+ y + " -c /dev/video0 -f rgb24 -F mono16 -r 3 -o " + x + ".avi"
+#debian 6
+        comando = "ffmpeg -f video4linux2 -s 640x480 -r ntsc -i /dev/video0 -f oss -i /dev/dsp -t 00:00:" + y + " -qscale 3 " + x + ".mpeg"
+        os.system(comando)
